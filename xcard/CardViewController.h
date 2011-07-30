227e23f27e36c97a228e2fef84aa1fd3a6159e7b
Ben <benbruscella@gmail.com>
2011-07-30 23:12:57 +1000

    buttons

---
+++ xcard/CardViewController.h
@@ -0,0 +1,16 @@
+//
+//  CardViewController.h
+//  xcard
+//
+//  Created by Ben Bruscella on 30/07/11.
+//  Copyright 2011 SPK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CardViewController : UIViewController {
+    
+}
+
+@end
